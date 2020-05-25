334d4cf5d1a56f9e22c1c6077a8276025280a2ca
nobasksio <a420673@yandex.ru>
2020-05-25 10:29:23 +0800

    increased axios timeout

--- client/src/http.js
+++ client/src/http.js
@@ -4,7 +4,7 @@ import store from './store/';
 export default () => {
     return axios.create ({
         baseURL: store.state.baseUrl,
-        timeout:3000,
+        timeout:10000,
         headers: {Authorization: `Bearer ${store.state.auth.tokenLogin}`}
     });
 }
